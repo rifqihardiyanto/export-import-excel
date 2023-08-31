9ce0c224f2699b4a9059ada85661bf0a11bf3033
rifqihardiyanto <rifqihardiyanto23@gmail.com>
2023-08-31 20:36:05 +0700

    edit

--- app/Exports/ClossingExport.php
+++ app/Exports/ClossingExport.php
@@ -12,7 +12,7 @@ class ClossingExport implements FromView
     public function view(): View
     {
         return view('dataclossings', [
-            'row' => Clossing::all()
+            'Clossing' => Clossing::all()
         ]);
     }
 }
